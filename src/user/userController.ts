5cb98b0cb972292df26c92423a56a6d17bed43d7
akash-plugseal <akash.kumar@plugseal.in>
2025-03-20 09:38:21 +0530

    add user register validation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,7 +1,20 @@
 import { NextFunction, Request, Response } from "express";
-
+import createHttpError from "http-errors";
+/**
+ * 1. Validation
+ * 2. Process
+ * 3. Response
+ */
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    res.json({ message: 'user created' })
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        const error = createHttpError(400, 'All field are required');
+        return next(error)
+    }
+
+
+    res.json({ message: 'user created', name, email, password });
 }
 
 export { createUser }
