e3f1914339ffae44461d9fa04176c64799c2a6ef
akash-plugseal <akash.kumar@plugseal.in>
2025-03-20 09:08:23 +0530

    create user router

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,17 @@ import express, { ErrorRequestHandler, NextFunction, Request, Response } from 'e
 import createHttpError, { HttpError } from 'http-errors';
 import { config } from './config/config';
 import globalErrorHandler from './middleware/globalErrorHandler';
+import userRouter from './user/userRouter';
 
 const app = express();
 app.use(express.json());
 
 //Routes
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({message: 'Hello World'});
+    res.status(200).json({message: 'Welcome to elib api'});
 })
+
+app.use('/api/users', userRouter);
 
 
 app.use(globalErrorHandler)
