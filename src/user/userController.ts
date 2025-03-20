414b553c9a4cb1db5b6dab9fcc8942f3e3d88934
akash-plugseal <akash.kumar@plugseal.in>
2025-03-20 10:15:19 +0530

    add hash

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import { UserModel } from "./userModel";
+import bcrypt from 'bcrypt'
 /**
  * 1. Validation
  * 2. Process
@@ -20,7 +21,9 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         return next(error)
     }
 
-    
+    const hashPassword = await bcrypt.hash(password, 10);
+
+
 
     res.json({ message: 'user created', name, email, password });
 }
